df8deda68485e20b596306656181b46f5debfdd9
Iago Suzart Silva <151882235+iaguinho-0@users.noreply.github.com>
2025-04-19 15:20:44 -0300

    Update Repository

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+  import type { NextConfig } from "next";
+
+  const nextConfig: NextConfig = {
+    /* config options here */
+    images: {
+      remotePatterns: [
+        {
+          hostname: "u9a6wmr3as.ufs.sh",
+        },
+        {
+          hostname: "qqkj4g40p2.ufs.sh",
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
